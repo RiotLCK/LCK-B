80a180488e56be373c17bcccafdfe1f42928c75a
EILE23 <tkdgys1234@gmail.com>
2025-07-03 21:43:23 +0900

    Initial commit

---
+++ src/main/kotlin/com/lckb/lck_backend/LckBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.lckb.lck_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LckBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<LckBackendApplication>(*args)
+}
